cdb17d6bf74e8afd1de38d43ea0ddc314a7e90d4
Shaneil Clarke <shaneilske@yahoo.com>
2019-03-01 12:04:54 +0000

    fixed rubocop offenses

--- spec/transactionHistory_spec.rb
+++ spec/transactionHistory_spec.rb
@@ -3,7 +3,7 @@ require 'transaction'
 
 describe TransactionHistory do
   let(:list) { described_class.new } 
-  let(:transaction) { double :transaction, withdraw: 200, balance:100, time: '14/01/2012' }
+  let(:transaction) { double :transaction, withdraw: 200, balance: 100, time: '14/01/2012' }
 
   describe "#no transaction" do
     it 'has initial empty transaction history' do
@@ -12,12 +12,12 @@ describe TransactionHistory do
     end
   end
  
-describe "#after transaction" do
-  it 'can record history of transactions' do
-    list.add_transaction(:transaction)
-    expect(list.history.length).to eq(1)
-    expect(list.history).to eq([:transaction])
+  describe "#after transaction" do
+    it 'can record history of transactions' do
+      list.add_transaction(:transaction)
+      expect(list.history.length).to eq(1)
+      expect(list.history).to eq([:transaction])
+    end
   end
-end
 
 end
